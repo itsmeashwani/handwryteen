3ef0f5dabea2372aa295715e455885ca7bb935b6
itsmeashwani <69910613+itsmeashwani@users.noreply.github.com>
2020-08-29 14:50:48 +0530

    Small change

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -132,7 +132,7 @@
                         </div>
                         <div class="modal-footer">
                         <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        <button type="submit" class="btn btn-primary">Add Trigger</button>
+                        <button type="submit" class="btn btn-primary" disabled>Add Trigger</button>
                         </div>
                     </div>
                     </form>
